b2da584d8abecca31ec69217f650cad796123aed
Yulia Nesmeyanova <69436822+Jnesmey@users.noreply.github.com>
2026-09-06 15:11:55 +0300

    Add files via upload

---
+++ Микроконтроллер/Test_1.py
@@ -0,0 +1,18 @@
+from machine import Pin
+import time 
+_init()
+ 
+LedPin_red = 16
+LedPin_green = 0
+ 
+led_red = Pin(LedPin_red, Pin.OUT)
+led_green = Pin(LedPin_green, Pin.OUT)
+ 
+while True:
+    led_red.off()
+    led_green.off()
+    time.sleep(2)
+    led_red.on()
+    time.sleep(2)
+    led_green.on()
+
